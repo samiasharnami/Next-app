b91bd857f6e835102f033d807d327ceef4f67b71
Samia Sharnami <161828593+samiasharnami@users.noreply.github.com>
2024-03-29 10:35:52 +0600

    routing and navigation

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -25,11 +25,7 @@ const NavMenu = () => {
                 </div>
             </div>
             <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-                <li>
-                <a className="justify-between">
-                    Profile
-                </a>
-                </li>
+                <li><a className="justify-between">Profile</a></li>
                 <li><a>Settings</a></li>
                 <li><a>Logout</a></li>
             </ul>
