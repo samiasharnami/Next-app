23c34bb8c65ff1b60b1d646aaeb1a4c513e78317
Samia Sharnami <161828593+samiasharnami@users.noreply.github.com>
2024-04-01 10:56:18 +0600

    uploading files

---
+++ app/upload/page.tsx
@@ -0,0 +1,39 @@
+'use client';
+import React, { useState } from 'react';
+import { CldUploadWidget, CldImage } from 'next-cloudinary';
+
+interface CloudinaryResult {
+    public_id: string;
+}
+
+const UploadPage = () => {
+
+    const [publicId, setPublicId] = useState('');
+
+  return (
+    <>
+        {publicId && 
+            <CldImage src={publicId} width={270} height={180} alt='demo pic'/>}
+        <CldUploadWidget 
+            uploadPreset='shnbqlxk' 
+            options={{
+                sources: [
+                    "local",
+                    "url",
+                    "camera",
+                    "google_drive"
+                ],
+                maxFiles: 5
+            }}
+            onUpload={(result, widget) => {
+                if (result.event !== 'success') return;
+                const info = result.info as CloudinaryResult;
+                setPublicId(info.public_id);
+        }}>
+            {({ open }) => <button className='btn btn-primary' onClick={() => open() }>Upload</button>}
+        </CldUploadWidget>
+    </>
+  )
+}
+
+export default UploadPage
